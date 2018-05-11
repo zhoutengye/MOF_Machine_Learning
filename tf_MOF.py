2a64b01377ce685deb0c80396e667b59c394feee
Yeld0n <yztchina@zju.edu.cn>
2018-05-11 14:54:37 -0400

    -a

--- tf_MOF.py
+++ tf_MOF.py
@@ -25,4 +25,22 @@ Centroid_y = f[5,:]
 
 MOF_Training = pd.DataFrame({ 'Norm X': Norm_x, 'Norm Y': Norm_y, 'Intersecpt': Intersecpt, 'Area': Area, 'Centroid X': Centroid_x, 'Centroid Y': Centroid_y})
 
-targets = MOF_Training["Area";"Centroid_x";"Centroid_y"]
+N_OUTPUTS = 3
+N_INPUTS = 3
+
+N_HIDDEN_UNITS =  
+N_EPOCHS =  
+
+input = tf.placeholder(tf.float32, shape=[None, N_INPUTS], name='input')  # input here
+
+outputs = tf.placeholder(tf.float32, shape=[None, N_OUTPUTS], name='output')  # one sample is something like[Ax,Ay,Az]
+
+# one hidden layer with 3 outputs
+W = {
+    'hidden': tf.Variable(tf.random_normal([N_INPUTS, N_HIDDEN_UNITS])),
+    'output': tf.Variable(tf.random_normal([N_HIDDEN_UNITS, N_OUTPUTS]))
+}
+biases = {
+    'hidden': tf.Variable(tf.random_normal([N_HIDDEN_UNITS], mean=1.0)),
+    'output': tf.Variable(tf.random_normal([N_OUTPUTS]), mean=1.0)
+}
